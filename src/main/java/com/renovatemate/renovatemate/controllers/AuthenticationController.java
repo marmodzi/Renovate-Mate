fa31d7a3640b5f4e94303537525f9440e799f83a
marmodzi <modzelewski.marcin@gmail.com>
2023-04-17 15:07:19 +0200

    controller and view added

---
+++ src/main/java/com/renovatemate/renovatemate/controllers/AuthenticationController.java
@@ -0,0 +1,40 @@
+package com.renovatemate.renovatemate.controllers;
+
+import com.renovatemate.renovatemate.entities.User;
+import com.renovatemate.renovatemate.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AuthenticationController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute("user") User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userService.save(user);
+        return "redirect:/login";
+    }
+}
+
