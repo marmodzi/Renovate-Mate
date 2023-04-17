b0a6d7df03af51cfaed2d4f92151f27f8ec20b96
marmodzi <modzelewski.marcin@gmail.com>
2023-04-17 11:54:02 +0200

    repositories changed

--- src/main/java/com/renovatemate/renovatemate/entities/User.java
+++ src/main/java/com/renovatemate/renovatemate/entities/User.java
@@ -14,7 +14,7 @@ public class User {
 
     private String email;
 
-    // email at this stage of application, not yet used
+    // at this stage of application, not yet used
 
     public User() {
     }
--- src/main/java/com/renovatemate/renovatemate/repositories/TaskToolRepository.java
+++ src/main/java/com/renovatemate/renovatemate/repositories/TaskToolRepository.java
@@ -6,7 +6,6 @@ import com.renovatemate.renovatemate.entities.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.scheduling.config.Task;
 
 import java.util.List;
 
