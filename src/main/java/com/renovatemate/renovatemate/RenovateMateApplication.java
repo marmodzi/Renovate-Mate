b1dd7c845bab098a4c1504809bdeaa6dd54447d2
marmodzi <modzelewski.marcin@gmail.com>
2023-04-17 10:29:13 +0200

    checkout commit

---
+++ src/main/java/com/renovatemate/renovatemate/RenovateMateApplication.java
@@ -0,0 +1,13 @@
+package com.renovatemate.renovatemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenovateMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RenovateMateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/renovatemate/renovatemate/RenovateMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renovatemate.renovatemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenovateMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
